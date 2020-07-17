33ecad688931b03ea0de3dd69ce089c7d66d0bc4
wugang <752820344@qq.com>
2020-07-17 18:55:21 +0800

    add 生产者-消费者模式案例

---
+++ src/main/java/com/bubble/pattern/observer/pc/EaterThread.java
@@ -0,0 +1,36 @@
+package com.bubble.pattern.observer.pc;
+
+import sun.tools.jconsole.Tab;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 表示来吃蛋糕的客人
+ *
+ * @author wugang
+ * date: 2020-07-16 20:00
+ **/
+public class EaterThread extends Thread {
+    private final Random random;
+    private final Table table;
+
+    public EaterThread(String name, Table table, long seed) {
+        super(name);
+        this.table = table;
+        this.random = new Random(seed);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String cake = table.take();
+                TimeUnit.MILLISECONDS.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
